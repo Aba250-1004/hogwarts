bd54fc5e8a46464c64f6a09848ad0a54b88dc4be
Amal Amchtal <amchtala@union.edu>
2020-09-08 19:36:43 -0400

    Year 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -8,11 +8,11 @@
 // set it to a variable named container
 const container = document.querySelector("#container");
 // console.log container
-console.log(container);
+// console.log(container);
 // Create an <h1> element and set it to a 
 //variable called h1 and console log it
 const h1 = document.createElement("h1")
-console.log(h1);
+// console.log(h1);
 // Add some text inside the h1 element. Example text: 'Hogwarts'
 h1.innerText = "Hogwarts";
 // Append your h1 to your container
@@ -43,3 +43,46 @@ container.appendChild(h4);
 const h42 = document.createElement("h4")
 h42.innerText = "Basilisk scale";
 container.appendChild(h42);
+
+
+// year 3
+const ul = document.createElement("ul");
+ul.setAttribute("data-storage","trunk");
+//console.log(ul);
+container.appendChild(ul);
+
+// butter beer
+const li1 = document.createElement("li");
+li1.innerText = "butter beer";
+ul.appendChild(li1);
+
+// invisibility cloak
+
+const li2 = document.createElement("li");
+li2.innerText = "invisibility cloak";
+li2.setAttribute("class","secret");
+// console.log(li2)
+ul.appendChild(li2);
+
+// magic map
+const li3 = document.createElement("li");
+li3.innerText = "magic map";
+li3.setAttribute("class","secret");
+ul.appendChild(li3);
+
+// time turner
+const li4 = document.createElement("li");
+li4.innerText = "time turner";
+li4.setAttribute("class","secret");
+ul.appendChild(li4);
+
+//leash
+const li5 = document.createElement("li");
+li5.innerText = "leash";
+li5.setAttribute("class","owl");
+ul.appendChild(li5);
+
+// Bertie Bott's Every Flavor [Jelly] Beans
+const li6 = document.createElement("li");
+li6.innerText = "Bertie Bott's Every Flavor Jelly Beans";
+ul.appendChild(li6);
