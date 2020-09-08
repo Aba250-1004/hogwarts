5c2c66ed95b93081a776f02decb5f397d86f78fc
Amal Amchtal <amchtala@union.edu>
2020-09-08 19:05:38 -0400

    Year 1 - added h1 using javascript

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    console.log('DOM is loaded!')
-})
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     console.log('DOM is loaded!')
+// })
+
+// year1 
+
+// Query for your div with the id of container and 
+// set it to a variable named container
+const container = document.querySelector("#container");
+// console.log container
+console.log(container);
+// Create an <h1> element and set it to a 
+//variable called h1 and console log it
+const h1 = document.createElement("h1")
+console.log(h1);
+// Add some text inside the h1 element. Example text: 'Hogwarts'
+h1.innerText = "Hogwarts";
+// Append your h1 to your container
+container.appendChild(h1)
+
