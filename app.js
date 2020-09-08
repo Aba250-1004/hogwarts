ee0e33c27d64e167a413addca6829f25be76190f
Amal Amchtal <amchtala@union.edu>
2020-09-08 19:20:05 -0400

    Year 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -18,3 +18,28 @@ h1.innerText = "Hogwarts";
 // Append your h1 to your container
 container.appendChild(h1)
 
+
+// year 2
+// h2 element with your name
+const h2 = document.createElement("h2");
+h2.innerText = "Amal Amchtal";
+// console.log(h2);
+container.appendChild(h2);
+// h3 element with your house (Hufflepuff, Gryffindor, 
+// Ravenclaw or Slytherin )
+const h3 = document.createElement("h3");
+h3.innerText = "Slytherin";
+container.appendChild(h3);
+// h4 element with your pet's name
+// this h4 element should have a class with a value of 
+// your pet type (owl, cat, toad, etc)
+const h4 = document.createElement("h4");
+h4.setAttribute("class","owl");
+h4.innerText = "Toby";
+container.appendChild(h4);
+// h4 element with your wand (You can make it whatever you want. 
+// Need help coming up with one? Here is one of many websites to 
+// help you find your wand playbuzz.com )
+const h42 = document.createElement("h4")
+h42.innerText = "Basilisk scale";
+container.appendChild(h42);
