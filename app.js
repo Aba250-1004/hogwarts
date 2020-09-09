17f621f7a139ad4610da1f3ce71ee6769396a32e
Amal Amchtal <amchtala@union.edu>
2020-09-08 20:34:23 -0400

    Year 5 - The days go by slowly and the years go by too fast

--- app.js
+++ app.js
@@ -172,3 +172,31 @@ tr4.appendChild(tdFD)
 
 tdFC.innerHTML = "Quitditch Practice";
 tr4.appendChild(tdFC)
+
+
+// year 5 
+// Break your wand! (select the element that contains 
+// your wand and remove it)
+h42.remove();
+// Class was hard! Drink all your butter beer! 
+// (remove just the butter beer from your list)
+li1.remove();
+// Get a new wand (add the same element back with 
+// new text describing your new wand. Be sure to insert
+//  it after your pet in the DOM)
+container.insertBefore(h42,ul);
+h42.innerHTML= "New Cooler Wand"
+// Make your new wand stand out by adding a color of 
+// indigo (or whatever color you like). But do it with magic 
+// (DOM manipulation): Don't add this css in your main.css file
+h42.style.color = "purple";
+// Send your pet on a spy mission (remove your pet 
+// from the DOM, put it somewhere else in your HTML). 
+// Make sure your pet's leash stays in your trunk (list 
+// item with the same class as your pet inside unordered list)
+h4.remove();
+container.appendChild(h4);
+// Have your pet come back (remove your pet from the
+// DOM, put it back in its original location)
+h4.remove();
+container.insertBefore(h4,h42);
